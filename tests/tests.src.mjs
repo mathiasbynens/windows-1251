0937402e2dd7fb5c38cbe420cdbbd33c35023dc9
Mathias Bynens <mathias@qiwi.be>
2021-09-05 08:37:03 +0200

    Fix `replacement` encoding mode
    Issue: https://github.com/mathiasbynens/windows-1252/issues/8

--- tests/tests.src.mjs
+++ tests/tests.src.mjs
@@ -50,6 +50,11 @@ assert.throws(
 	Error,
 	'Mode names are case-insensitive'
 );
+assert.deepStrictEqual(
+	windows1251.encode('\uFFFF', { mode: 'replacement' }),
+	new Uint16Array([0xFFFD]),
+	'Encoding a code point that is invalid for this encoding results in U+FFFD in `replacement` mode'
+);
 
 console.log('Testing `windows1251.decode`…');
 assert.deepStrictEqual(
